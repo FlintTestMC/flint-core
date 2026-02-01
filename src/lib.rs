720c07a5e2785dd409b1aa934e6a374745e1cb75
JunkyDeveloper <junkydeveloper@gmail.com>
2026-02-01 02:57:21 +0100

    removed unused files

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,14 @@ pub mod format;
 pub mod index;
 pub mod loader;
 pub mod results;
+pub mod runner;
 pub mod spatial;
 pub mod test_spec;
 pub mod timeline;
-pub mod utils;
-pub mod filter;
-pub mod mock;
-pub mod runner;
 pub mod traits;
+pub mod utils;
 
 // Re-export main types for convenience
-pub use filter::{TestFilter, TestSelector};
-pub use mock::{MockAdapter, MockPlayer, MockWorld};
 pub use runner::{TestRunConfig, TestRunner};
 pub use traits::{BlockPos, FlintAdapter, FlintPlayer, FlintWorld, Item, ServerInfo};
 
