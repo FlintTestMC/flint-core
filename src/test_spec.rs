9658d4f170431d8637fc19644acc9969db028f26
JunkyDeveloper <junkydeveloper@gmail.com>
2026-02-09 19:43:06 +0100

    fix

--- src/test_spec.rs
+++ src/test_spec.rs
@@ -22,8 +22,6 @@ pub struct TestSpec {
     pub setup: Option<SetupSpec>,
     pub timeline: Vec<TimelineEntry>,
     #[serde(default)]
-    pub minecraft_ids: Vec<String>,
-    #[serde(default)]
     pub breakpoints: Vec<u32>,
 }
 
